5bc06638f9422eaf79de6cc8bd6f4115866e7b5c
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-01-29 14:53:34 -0700

    Agregar archivos de proyecto.

---
+++ Assets/Scripts/Core/Movement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Movement
+{
+    public class Movement
+    {
+        /// <summary>
+        /// This function returns a Vector2 Axis of horizontal and vertical.
+        /// </summary>
+
+        public static Vector2 Axis
+        {
+            get
+            {
+                return new Vector2(Input.GetAxis("Horizontal"),
+                Input.GetAxis("Vertical"));
+            }
+        }
+
+        /// <summary>
+        /// This function returns a Vectoer2 Axis of horizontal and vertical multiplied by delta time
+        /// </summary>
+
+        public static Vector2 AxisDelta
+        {
+            get
+            {
+                return new Vector2(Input.GetAxis("Horizontal"),
+                Input.GetAxis("Vertical")) * Time.deltaTime;
+            }
+        }
+
+
+        /// <summary>
+        /// Moves the player in Horizontal multiplied by speed variable
+        /// </summary>
+        /// <param name="t">Transform component of the player</param>
+        /// <param name="speed">The speed movement of the player</param>
+        public static void SimpleMovement(Transform t, float speed)
+            {
+            t.Translate(Vector2.right * Axis.x * speed);
+            }
+
+
+        /// <summary>
+        /// Moves the player in Horizontal multiplied by speed variable
+        /// And delta time
+        /// </summary>
+        /// <param name="t">Transform component of the player</param>
+        /// <param name="speed">The speed movement of the player</param>
+        public static void DeltaMovement(Transform t, float speed)
+        {
+            t.Translate(Vector2.right * AxisDelta.x * speed);
+        }
+    }
+}
+
+
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Movement;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 0f;
+
+    Animator anim;
+    SpriteRenderer spr;
+
+    float moveX;
+
+    [SerializeField]
+    AudioClip sfx_coin;
+
+    AudioSource auds;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
+        auds = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        moveX = Movement.AxisDelta.x;
+
+        /*
+        transform.Translate(Vector3.right * 
+            moveX * 
+            moveSpeed * Time.deltaTime);
+        */
+
+        Movement.SimpleMovement(transform, moveSpeed);
+
+        anim.SetFloat("MoveX", Mathf.Abs(moveX));
+
+        spr.flipX = moveX < 0f ? true : moveX > 0f ? false : spr.flipX;
+
+        
+    }
+
+    public void PlayCoinSFX()
+    {
+        auds.PlayOneShot(sfx_coin, 7f);
+    }
+
+
+
+}
